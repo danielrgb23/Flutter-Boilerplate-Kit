db3f0e605053cd5123456e9444e7e05ac0c34b2c
Daniel Jasmelino <danielrgb2@gmail.com>
2025-04-24 14:43:55 -0300

    chore: removendo getx & toast

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:bot_toast/bot_toast.dart';
 import 'package:config_flutter/pages/teste.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 import 'package:config_flutter/inherited/messaging.dart';
 import 'package:config_flutter/l10n/app_localizations.dart';
 import 'package:config_flutter/utils/navigator_service.dart';
 import 'package:config_flutter/common/mixins/l10n_mixin.dart';
+
 class FlutterConfigApp extends StatelessWidget with L10n {
   const FlutterConfigApp({super.key});
 
   Widget _app() {
-    return GetMaterialApp(
+    return MaterialApp(
       builder: (BuildContext context, Widget? widget) {
         ErrorWidget.builder = (FlutterErrorDetails errorDetails) {
           return Container(
             padding: const EdgeInsets.all(20),
             child: Center(
               child: Text(
-                'Ocorreu um error',
+                'Ocorreu um erro',
                 style: const TextStyle(color: Color(0xFF9CA0A6), fontSize: 20),
               ),
             ),
           );
         };
         L10n.init(context);
-        final botFunc = BotToastInit();
-
-        return botFunc(context, widget);
+        return widget!;
       },
       navigatorKey: NavigatorService.key,
       supportedLocales: AppLocalizations.supportedLocales,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
-      navigatorObservers: [
-        BotToastNavigatorObserver(),
-      ],
       debugShowCheckedModeBanner: false,
       onGenerateTitle: (context) {
         final l10n = AppLocalizations.of(context)!;
-
         return l10n.flutterConfigApp;
       },
-
+      initialRoute: Teste.PATH,
       routes: {
-        Teste.PATH: (context) => Teste()
+        Teste.PATH: (context) => Teste(),
       },
     );
   }
